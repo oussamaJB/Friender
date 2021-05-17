4e3a0209374da72227dc294b1df241fb0bee23ad
oussamaJB <oussama.jaballah9@gmail.com>
2021-05-17 22:04:23 +0100

    adding image upload

---
+++ src/app/authenticate.service.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './User';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticateService {
+  public access = false;
+
+// sign in
+  exist() {
+    this.access = true;
+    this.router.navigate(['/home']);
+  }
+  invalid(){
+    alert(this.u.error);
+  }
+
+  // signup
+  public email;
+  public name;
+  public pwd;
+  signup(email,name,pwd){
+    this.email=email;
+    this.name=name;
+    this.pwd=pwd;
+
+    this.http.get(
+      "http://guinea-pig.ddns.net:5000/api/add/user?"+
+      "username="+this.name+"&password="+this.pwd+"&email="+this.email).subscribe();
+    alert(this.name  +" : Your are registred");
+  }
+  getuser():User{ return this.u; }//pour oussema
+
+  public _url = '';
+  public u;
+  constructor(private http: HttpClient, private router: Router) {}
+  getUser(): Observable<User>   {
+    this.http.get(this._url).toPromise().then(data => {
+      this.u = data;
+      if (this.u.success){ this.exist();}
+      else  this.invalid();
+
+    })
+    return this.http.get<User>(this._url);
+
+  }
+}
